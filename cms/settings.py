2c2bf47dd7fca997b43828d9dd337e1d536754fa
Adib M <adibw449@gmail.com>
2025-01-01 16:30:48 +0530

    Added login form and view

--- cms/settings.py
+++ cms/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cms_db',
+        'USER' : 'postgres',
+        'PASSWORD' : '852013',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
--- cms_app/views.py
+++ cms_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username = username , password = password)
+
+        if user is not None:
+            login(request,user)
+
+            if user.role == 'ADMIN':
+                return redirect('admin_dashboard')
+            
+            elif user.role == 'INSTRUCTOR':
+                return redirect('instructor_dashboard')
+            
+            else:
+                return redirect('student_dashboard')
+        else:
+            messages.error(request,'Invalid Credentials')
+        
+        return render(request,'cms/login.html')
+
