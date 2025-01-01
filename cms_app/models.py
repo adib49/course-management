dd6e3d2fc2baa45e3d80d6608de42af4558c52ab
Adib M <adibw449@gmail.com>
2025-01-01 13:38:47 +0530

    Project Structure Defined

---
+++ cms_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    ROLES = (
+        ('ADMIN','Admin'),
+        ('INSTRUCTOR','Instructor'),
+        ('STUDENT','Student')
+    )
+    
+    role = models.CharField(max_length = 20, choices = ROLES)
+    user_id = models.CharField(max_length = 5, unique = True)
+
+class Course(models.Model):
+    title = models.CharField(max_length = 30)
+    content = models.CharField(max_length = 200)
+    course_code = models.CharField(max_length = 5, unique=True )
+    created_at = models.DateTimeField(auto_now_add= True)
+    instructor = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name='courses')
+    student = models.ForeignKey(CustomUser,on_delete= models.CASCADE, related_name = 'enrolled_courses')
+    
